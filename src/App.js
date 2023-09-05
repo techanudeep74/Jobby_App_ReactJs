5ce3231c4d6eaf722da46ec4fafda2e780b2368e
techanudeep74 <4a1vnp6@gmail.com>
2023-09-05 13:42:30 +0000

    Jobby-APP

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import JobItemDetails from './components/JobItemDetails'
+import AllJobs from './components/AllJobs'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/jobs" component={AllJobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
+  </Switch>
+)
+
+export default App
